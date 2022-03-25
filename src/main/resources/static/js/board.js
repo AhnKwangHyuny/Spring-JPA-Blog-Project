69d96d87a1dab7e91c597e1cfc6afe5a14da03c9
AHNKWANGHYUN <agh0314@gmail.com>
2022-03-25 14:19:46 +0900

    유저 프로파일 이미지 저장 로직 구현 전

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -12,8 +12,14 @@ let index = {
 
             $("#button-board-update").on("click" , ()=> {
                 this.update();
-            })
+            });
+
+            $("#btn-board-delete").on("click" , ()=> {
+                this.deleteBoard();
+            });
     },
+
+    //== board save ==//
     save: function() {
 
         const newBoardData = {
@@ -40,36 +46,75 @@ let index = {
         });
     },
 
+    //== board update ==//
     update: function() {
 
-            const boardId = $("#board_id").val();
+        const boardId = $("#board_id").val();
 
-            const updatedBoardData = {
-                content : $("#summernote").summernote('code'),
-                title : $("#title").val(),
-                userId : $("#userId").val(),
-                boardId : boardId,
-            };
+        const updatedBoardData = {
+            content : $("#summernote").summernote('code'),
+            title : $("#title").val(),
+            userId : $("#userId").val(),
+            boardId : boardId,
+        };
 
-            $.ajax({
-                url : `/boards/`+ boardId + `/update`,
-                type : 'PUT',
-                data : JSON.stringify(updatedBoardData), // data format -> json
-                processData: false,
-                contentType: "application/json; charset=utf-8", // http body data 형식
-                dataType : "text"
-            }).done(function(res){ // javscript object로 변환하여 res에 담아줌
+        $.ajax({
+            url : `/boards/`+ boardId + `/update`,
+            type : 'PUT',
+            data : JSON.stringify(updatedBoardData), // data format -> json
+            processData: false,
+            contentType: "application/json; charset=utf-8", // http body data 형식
+            dataType : "text"
+        }).done(function(res){ // javscript object로 변환하여 res에 담아줌
 
-                console.log(res);
-                alert("성공적으로 수정되었습니다!");
-                location.href="/boards/" + boardId;
+            console.log(res);
+            alert("성공적으로 수정되었습니다!");
+            location.href="/boards/" + boardId;
 
-            }).fail(function(error){
-                console.log(error);
-                alert("글 수정에 실패 했습니다.");
-//                location.href="/boards/"+ boardId + "/update";
-            });
+        }).fail(function(error){
+            console.log(error);
+            alert("글 수정에 실패 했습니다.");
+            location.href="/boards/"+ boardId + "/update";
+        });
+    },
+
+    //== board delete ==//
+    deleteBoard: function(){
+
+        const boardId = $("#boardId").val();
+        const passwordData = {
+            password : $("#password").val(),
         }
+
+        $.ajax({
+            url : `/boards/`+ boardId,
+            type : 'DELETE',
+            data : JSON.stringify(passwordData), // data format -> json
+            processData: false,
+            contentType: "application/json; charset=utf-8", // http body data 형식
+            dataType : "text"
+        }).done(function(res){ // javscript object로 변환하여 res에 담아줌
+
+            console.log(res);
+            alert("성공적으로 삭제되었습니다!");
+            location.href="/";
+
+        }).fail(function(jqXHR , error , textStatus){
+            console.log(jqXHR.status)
+            let errorMessage;
+            let errorMessageContainer = $(".errorMessage-container");
+            if(jqXHR.status === 400){
+
+                // 이미 비밀번호 에러 메세지가 있을경우 기존의 메세지 사용 (새로 만들지 않음)
+                if(errorMessageContainer.children().length > 0) return;
+                // 처음 비밀번호 입력에 실패 했을 경우
+                $("#password").val("");
+                errorMessage = "<p class = 'alert alert-danger'>" + "비밀번호가 일치하지 않습니다 " + "</p>";
+                errorMessageContainer.append(errorMessage);
+            }
+
+        });
+    }
 };
 
 
@@ -100,7 +145,29 @@ $(document).ready(()=> {
    $("#title").on("keyup" , ()=> {
         buttonDisabledAttrSetting(title ,removeInitialTagInSummernoteEditor(content));
    });
+
+   // 게시글 삭제 버튼 disabled 유무
+   let password;
+   let deleteButton = $("#btn-board-delete");
+
+    $("#password").on("keyup" , ()=> {
+        password = $("#password").val();
+
+        if(password.length != 0){
+            deleteButton.attr("disabled" ,false);
+            return;
+        }
+
+    // 닫기 버튼 눌렀을 때 모달 안에 입력된 내용 삭제
+    $("#btn-close-delete").on("click", ()=> {
+        document.getElementById("password").value="";
+    });
+
+    deleteButton.attr("disabled" , true);
+   });
 });
+
+
 
 // button disabled 사용 유무 함수
 function buttonDisabledAttrSetting (title , content) {
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,39 +1,122 @@
+// Global Variables
+let cropper;
+
 // 회원가입 내용 json 형식으로 서버에 보내기
-let index = {
-    init: function() { // function()이 아닌 ()=> 쓴 이유 : this를 binding 하기 위해
-            $("#button-save").on("click" , () => {
-                this.save();
+//let index = {
+//    init: function() { // function()이 아닌 ()=> 쓴 이유 : this를 binding 하기 위해
+//            $("#button-save").on("click" , () => {
+//                this.save();
+//            });
+//    },
+//    save: function() {
+//
+//        const newUserData = {
+//            username : $("#username").val(),
+//            email : $("#email").val(),
+//            password : $("#password").val()
+//        };
+//
+//        // ajax호춯시 default가 비동기 호출
+//        // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
+//        $.ajax({
+//            url : `/auth/join`,
+//            type : 'POST',
+//            data : JSON.stringify(newUserData), // data format -> json
+//            processData: false,
+//            contentType: "application/json; charset=utf-8", // http body data 형식
+//        }).done(function(res){ // javscript object로 변환하여 res에 담아줌
+//
+//            console.log(res);
+//            alert("회원가입이 완료되었습니다.");
+//            location.href="/";
+//
+//        }).fail(function(error){
+//            console.log(error);
+//            alert("회원가입에 실패했습니다.");
+////            location.href="/auth/join";
+//        });
+//    }
+//};
+
+
+/*
+    user profile Image cropper
+*/
+
+$(document).ready(()=> {
+/*image upload modal*/
+    $('#filePhoto').change(event => {
+        // jquery object have not files property ...
+        //  take files property from event target object
+        let input = $(event.target);
+        if(input[0].files && input[0].files[0]){
+
+            let reader = new FileReader();
+
+            // when successfully file was loaded executes onload handler
+            reader.onload = (e) => {
+                $('#imageUploadButton').removeAttr('disabled');
+                let image = document.getElementById('imagePreview');
+                image.src = e.target.result;
+
+                if(cropper !== undefined){
+                    // Destroy the cropper and remove the instance from the image.
+                    // destroy method is a cropper method
+                    cropper.destroy();
+                }
+
+                cropper = new Cropper(image , {
+                    aspectRatio : 1/1,
+                    background : false,
+                    zoomOnWheel: false
+                });
+
+            }
+            /*The readAsDataURL method is used to read the contents of
+            the specified Blob or File. When the read operation is finished,
+            the readyState becomes DONE, and the loadend is triggered. At that time,
+            the result attribute contains the data as a data: URL representing the file's data as a base64 encoded string.*/
+            reader.readAsDataURL(input[0].files[0]);
+        }
+    });
+
+    // user profile image upload logic
+    $("#imageUploadButton").on('click' , ()=> {
+        let canvas = cropper.getCroppedCanvas();
+        // Upload cropped image to server if the browser supports `HTMLCanvasElement.toBlob`.
+        // The default value for the second parameter of `toBlob` is 'image/png', change it if necessary.
+
+        if(canvas == null || canvas == undefined ) {
+            alert('Could not upload Image. Make sure it is an image file.');
+            return;
+        }
+
+        canvas.toBlob( blob => {
+            console.log(blob);
+            const imageFormData = new FormData();
+
+            // Pass the image file name as the third parameter if necessary.
+            imageFormData.append('croppedImage', blob);
+
+            $.ajax({
+                url : `/app/users/profilePicture`,
+                type : 'POST',
+                data : imageFormData,
+                processData: false,
+                contentType: false,
+                success() {
+                    console.log('Upload success');
+                    location.reload();
+                },
+                error() {
+                console.log('Upload error');
+                }
             });
-    },
-    save: function() {
-
-        const newUserData = {
-            username : $("#username").val(),
-            email : $("#email").val(),
-            password : $("#password").val()
-        };
-
-        // ajax호춯시 default가 비동기 호출
-        // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
-        $.ajax({
-            url : `/auth/join`,
-            type : 'POST',
-            data : JSON.stringify(newUserData), // data format -> json
-            processData: false,
-            contentType: "application/json; charset=utf-8", // http body data 형식
-        }).done(function(res){ // javscript object로 변환하여 res에 담아줌
-
-            console.log(res);
-            alert("회원가입이 완료되었습니다.");
-            location.href="/";
-
-        }).fail(function(error){
-            console.log(error);
-            alert("회원가입에 실패했습니다.");
-//            location.href="/auth/join";
         });
-    }
-};
+    });
+});
+
+
 
 // 회원가입 버튼 클릭
 index.init();
